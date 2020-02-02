cb183e2cec7cb4d9a068838e5b7f46980c4ee710
Stephen Haslett <stephen.haslett07@spsmail.cuny.edu>
2020-02-02 03:31:46 +0000

    Final version of assignment 1

--- 01_assignment.py
+++ 01_assignment.py
@@ -5,7 +5,7 @@ Assignment #1
 2. Run the associated test harness for a basic check on completeness. A successful run of the test cases does not guarantee accuracy or fulfillment of the requirements. Please do not submit your work if test cases fail.
 3. To run unit tests simply use the below command after filling in all of the code:
     python 01_assignment.py
-  
+
 4. Unless explicitly stated, please do not import any additional libraries but feel free to use built-in Python packages
 5. Submissions must be a Python file and not a notebook file (i.e *.ipynb)
 6. Do not use global variables
@@ -33,6 +33,8 @@ def exercise01():
 
     # ------ Place code below here \/ \/ \/ ------
 
+    x = 5
+
     # ------ Place code above here /\ /\ /\ ------
 
     return x
@@ -42,6 +44,8 @@ def exercise02():
     # Create a string variable called name and set it to your first name.
 
     # ------ Place code below here \/ \/ \/ ------
+
+    name = 'Stephen'
 
     # ------ Place code above here /\ /\ /\ ------
 
@@ -53,6 +57,8 @@ def exercise03():
 
     # ------ Place code below here \/ \/ \/ ------
 
+    sentence = 'The rat the cat the dog chased killed ate the malt.'
+
     # ------ Place code above here /\ /\ /\ ------
 
     return sentence
@@ -63,6 +69,9 @@ def exercise04():
 
     # ------ Place code below here \/ \/ \/ ------
 
+    first_name = 'Stephen'
+    last_name = 'Haslett'
+
     # ------ Place code above here /\ /\ /\ ------
 
     return first_name, last_name
@@ -70,9 +79,13 @@ def exercise04():
 
 def exercise05():
 
-    # Repeate exercise 4 here and assign the datatype of the variable first_name to a variable called name_type
+    # Repeat exercise 4 here and assign the datatype of the variable first_name to a variable called name_type
 
     # ------ Place code below here \/ \/ \/ ------
+
+    first_name = 'Stephen'
+    last_name = 'Haslett'
+    name_type = type(first_name)
 
     # ------ Place code above here /\ /\ /\ ------
 
@@ -83,7 +96,9 @@ def exercise06():
     # Assign 20 to the variable hours_worked, 15 to the variable wage_per_hour and the product of the two to variable total_pay
 
     # ------ Place code below here \/ \/ \/ ------
-
+    hours_worked = 20
+    wage_per_hour = 15
+    total_pay = hours_worked * wage_per_hour
     # ------ Place code above here /\ /\ /\ ------
     return hours_worked, wage_per_hour, total_pay
 
@@ -91,7 +106,10 @@ def exercise06():
 def exercise07():
     # Create a variable wage and assign 17.0 to it. Print to the screen the datatype of wage. Create a second variable called doubled that prints to the screen 2 times wage
     # ------ Place code below here \/ \/ \/ ------
-
+    wage  = 17.0
+    print(type(wage))
+    doubled  = 2 * wage
+    print(doubled)
     # ------ Place code above here /\ /\ /\ ------
     return wage, doubled
 
@@ -100,7 +118,9 @@ def exercise08():
     # Assign 5 to the variable quantity, 'hello' to the variable hello and a variable hello_repeated that holds a string that contains whatever is contained in the variable hello repeated quantity times
 
     # ------ Place code below here \/ \/ \/ ------
-
+    quantity = 5
+    hello = 'hello'
+    hello_repeated = hello * quantity
     # ------ Place code above here /\ /\ /\ ------
 
     return quantity, hello, hello_repeated
@@ -110,7 +130,9 @@ def exercise09():
     # Assign 10 to a variable qty, 5 to a variable price and the product of the two to a variable total_cost
 
     # ------ Place code below here \/ \/ \/ ------
-
+    qty = 10
+    price = 5
+    total_cost = qty * price
     # ------ Place code above here /\ /\ /\ ------
 
     return qty, price, total_cost
@@ -120,7 +142,12 @@ def exercise10():
     # Create 5 variables named factorN where N is the numbers 1 to 5 and set them to 1 through 5, respectively. Create a variable called product that holds the product of the 5 variables
 
     # ------ Place code below here \/ \/ \/ ------
-
+    factor1 = 1
+    factor2 = 2
+    factor3 = 3
+    factor4 = 4
+    factor5 = 5
+    product = factor1 * factor2 * factor3 * factor4 * factor5
     # ------ Place code above here /\ /\ /\ ------
 
     return factor1, factor2, factor3, factor4, factor5, product
@@ -130,7 +157,7 @@ def exercise11():
     # Create a variable pi and literally set it to pi 10 decimal places out
 
     # ------ Place code below here \/ \/ \/ ------
-
+    pi = 3.1415926535
     # ------ Place code above here /\ /\ /\ ------
     return pi
 
@@ -139,7 +166,8 @@ def exercise12():
     # Create a variable called x and set it to 10. Then create a variable y that equals to x to the 7th power
 
     # ------ Place code below here \/ \/ \/ ------
-
+    x = 10
+    y = x ** 7
     # ------ Place code above here /\ /\ /\ ------
     return x, y
 
@@ -149,6 +177,8 @@ def exercise13():
 
     pi = 3.14159
     # ------ Place code below here \/ \/ \/ ------
+    r = 7
+    volume_sphere = round(4/3 * pi * r ** 3, 2)
 
     # ------ Place code above here /\ /\ /\ ------
     return pi, r, volume_sphere
@@ -159,6 +189,11 @@ def exercise14():
 
     # ------ Place code below here \/ \/ \/ ------
 
+    length = 50
+    height = 10.2
+    area = length * height
+    area_type = type(area)
+
     # ------ Place code above here /\ /\ /\ ------
     return area, length, height, area_type
 
@@ -167,7 +202,9 @@ def exercise15():
     # Calculate the distance covered by a car moving at 80 miles per hour for 3 hours
 
     # ------ Place code below here \/ \/ \/ ------
-
+    speed_mph = 80
+    duration = 3
+    distance = speed_mph * duration
     # ------ Place code above here /\ /\ /\ ------
 
     return distance, speed_mph, duration
@@ -177,7 +214,9 @@ def exercise16():
     # Implement pythogorean thereom and find the length of hypotenuse c given sides a and b. Select any numbers for a and b. Use math.sqrt() for square root.
 
     # ------ Place code below here \/ \/ \/ ------
-
+    a = 6
+    b = 3
+    c = math.sqrt(a ** 2 + b ** 2)
     # ------ Place code above here /\ /\ /\ ------
     return a, b, c
 
